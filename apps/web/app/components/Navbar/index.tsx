770b0091dfe1c6a31deb1b4115704ae9dc14a444
ridopurnomo <ridopurnomo86@gmail.com>
2024-07-31 13:38:47 +0700

    fix:useLayoutEffect drawer

--- apps/web/app/components/Navbar/index.tsx
+++ apps/web/app/components/Navbar/index.tsx
@@ -1,3 +1,5 @@
+import { useLocation } from "@remix-run/react";
+import { ClientOnly } from "remix-utils/client-only";
 import MainNavigation from "./MainNavigation";
 import EntryNavigation from "./EntryNavigation";
 import Drawer from "./Drawer";
@@ -7,14 +9,20 @@ type NavbarPropsType = {
   name: string;
 };
 
-const Navbar = ({ isAuthenticated, name }: NavbarPropsType) => (
-  <header className="relative border-b">
-    <div className="flex w-full items-center justify-between bg-transparent px-10 py-4 max-[894px]:px-8">
-      <MainNavigation />
-      <EntryNavigation isAuthenticated={isAuthenticated} name={name} />
-      <Drawer isAuthenticated={isAuthenticated} name={name} />
-    </div>
-  </header>
-);
+const Navbar = ({ isAuthenticated, name }: NavbarPropsType) => {
+  const location = useLocation();
+
+  return (
+    <header className="relative border-b">
+      <div className="flex w-full items-center justify-between bg-transparent px-10 py-4 max-[894px]:px-8">
+        <MainNavigation />
+        <EntryNavigation isAuthenticated={isAuthenticated} name={name} />
+        <ClientOnly fallback={null} key={location.pathname}>
+          {() => <Drawer isAuthenticated={isAuthenticated} name={name} />}
+        </ClientOnly>
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
