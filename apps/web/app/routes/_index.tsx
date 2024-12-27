8b55422989d7fabff579126e9220d53496668ea7
ridopurnomo <ridopurnomo86@gmail.com>
2024-12-27 11:08:14 +0700

    feat:create meta for homepage, event detail

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -4,8 +4,26 @@ import MainHome from "~/pages/MainHome";
 import { MainHomeLoader } from "services/main/main-home";
 
 export const meta: MetaFunction = () => [
-  { title: "Elastic Pass" },
-  { name: "description", content: "Welcome to Remix!" },
+  { title: "ElasticPass: Create Free Events, Organize Events & Sell Your Own Events" },
+  {
+    name: "description",
+    content:
+      "Now you can create events, organize events, sell event tickets yourself, create event invitations online and have the opportunity to be promoted by ElasticPass affiliates.",
+  },
+  { name: "keywords", content: "create event, organize event, sell event" },
+  { name: "author", content: process.env.HOSTNAME },
+  { name: "type", content: "website" },
+  {
+    property: "og:title",
+    content: "ElasticPass: Create Free Events, Organize Events & Sell Your Own Events",
+  },
+  { name: "og:type", content: "website" },
+  { name: "og:url", content: process.env.HOSTNAME },
+  {
+    name: "og:description",
+    content:
+      "Now you can create events, organize events, sell event tickets yourself, create event invitations online and have the opportunity to be promoted by ElasticPass affiliates.",
+  },
 ];
 
 export const loader = async (params: LoaderFunctionArgs) => await MainHomeLoader(params);
--- apps/web/app/routes/event/$slug/_route.tsx
+++ apps/web/app/routes/event/$slug/_route.tsx
@@ -3,7 +3,37 @@ import { EventDetailAction, EventDetailLoader } from "services/main/event/event-
 import Mainlayout from "~/components/layout/MainLayout";
 import EventDetailPage from "~/pages/Event/EventDetail";
 
-export const meta: MetaFunction = () => [{ title: "Event" }];
+const renderDescriptionContent = (title: string) =>
+  `See available schedules, locations and ticket prices for ${title.toUpperCase()}. Buy tickets or order online only at ElasticPass`;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const meta: MetaFunction = ({ data, location }: any) => [
+  { title: data.eventDetail.name },
+  { name: "keywords", content: "ElasticPass" },
+  { name: "author", content: process.env.HOSTNAME },
+  { name: "type", content: "article" },
+  { name: "og:site_name", content: "ElasticPass" },
+  {
+    property: "og:title",
+    content: data.eventDetail.name,
+  },
+  {
+    name: "description",
+    content: renderDescriptionContent(data.eventDetail.name),
+  },
+  {
+    name: "og:description",
+    content: renderDescriptionContent(data.eventDetail.name),
+  },
+  {
+    name: "og:image",
+    content: data.eventDetail.image_url,
+  },
+  {
+    name: "og:url",
+    content: `${process.env.HOSTNAME}${location.pathname}`,
+  },
+];
 
 export const action = async (params: ActionFunctionArgs) => await EventDetailAction(params);
 
