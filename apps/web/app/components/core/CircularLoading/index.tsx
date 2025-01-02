502e4f22c3e491a211f2b81962d5a63f5a84a7ce
ridopurnomo <ridopurnomo86@gmail.com>
2025-01-02 11:37:16 +0700

    feat:adjust circular loading, and navigation profile, project loader

--- apps/web/app/components/core/CircularLoading/index.tsx
+++ apps/web/app/components/core/CircularLoading/index.tsx
@@ -1,8 +1,9 @@
-const CircularLoading = () => (
+const CircularLoading = ({ size = 8, className }: { size?: number; className?: string }) => (
   <div role="status">
     <svg
       aria-hidden="true"
-      className="size-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+      // eslint-disable-next-line tailwindcss/no-custom-classname
+      className={`size-${size} animate-spin fill-blue-600 text-gray-200 dark:text-gray-600 ${className}`}
       viewBox="0 0 100 101"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
