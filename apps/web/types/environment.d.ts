1f0d6b8e002a87cf7b22e8c2941122c5d02cabc6
Ridho Purnomo <ridopurnomo86@gmail.com>
2025-02-24 10:17:34 +0700

    fix:remove database url & schema inside web

--- apps/web/types/environment.d.ts
+++ apps/web/types/environment.d.ts
@@ -2,7 +2,6 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: "development" | "production";
-      DATABASE_URL: string;
       SESSION_SECRET: string;
       BOOKING_SESSION_SECRET: string;
       REDIS_URL: string;
